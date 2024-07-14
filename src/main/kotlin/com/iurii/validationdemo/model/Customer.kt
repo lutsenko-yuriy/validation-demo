172cd163cd830a06265c5539f84b999afa0c12e3
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 00:58:36 +0200

    Make free passes field necessary

--- src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
+++ src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
@@ -8,12 +8,16 @@ import jakarta.validation.constraints.Size
 
 data class Customer(
     var firstName: String? = null,
+
     @field:NotNull(message = "is required")
     @field:Size(min = 1, message = "is required")
     var lastName: String? = null,
+
+    @field:NotNull(message = "is required")
     @field:Min(value = 0, message = "Must be greater than or equal to 0")
     @field:Max(value = 10, message = "Must be less than or equal to 10")
     var freePasses: Int? = null,
+
     @field:Pattern(regexp = "^[a-zA-Z0-9]{5}$", message = "Only 5 digits/letters")
     var postalCode: String? = null,
 )
