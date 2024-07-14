fb3d2568b6fa51fcf0061c9740a7a9601404909b
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 00:27:41 +0200

    Fix showing error

--- src/main/kotlin/com/iurii/validationdemo/controller/CustomerController.kt
+++ src/main/kotlin/com/iurii/validationdemo/controller/CustomerController.kt
@@ -1,15 +1,32 @@
 package com.iurii.validationdemo.controller
 
 import com.iurii.validationdemo.model.Customer
+import jakarta.validation.Valid
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class CustomerController {
+
     @GetMapping("/")
     fun showForm(model: Model): String {
         model.addAttribute("customer", Customer())
         return "customer/form"
+    }
+
+    @RequestMapping("/register")
+    fun register(
+        @Valid @ModelAttribute("customer") customer: Customer,
+        bindingResult: BindingResult,
+    ): String {
+        return if (bindingResult.hasErrors()) {
+            "customer/form"
+        } else {
+            "customer/register"
+        }
     }
 }
--- src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
+++ src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size
 
 data class Customer(
     var firstName: String? = null,
-    @NotNull(message = "is required")
-    @Size(min = 1, message = "is required")
-    var lastName: String = "",
+    @field:NotNull(message = "is required")
+    @field:Size(min = 1, message = "is required")
+    var lastName: String? = null,
 )
