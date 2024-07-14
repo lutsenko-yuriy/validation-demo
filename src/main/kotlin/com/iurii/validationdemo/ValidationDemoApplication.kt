1432a5a57b53366dd2a6efb53d7420b7da86aaea
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-14 23:25:11 +0200

    Initial commit

---
+++ src/main/kotlin/com/iurii/validationdemo/ValidationDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.iurii.validationdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidationDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ValidationDemoApplication>(*args)
+}
