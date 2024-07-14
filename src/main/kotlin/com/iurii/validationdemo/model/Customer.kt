fa886059a6eeef8a4ef6fce324f37e4805bd0576
Yuriy Lutsenko <lutsenko.yuriy93@gmail.com>
2024-07-15 00:55:48 +0200

    Add postal code

--- src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
+++ src/main/kotlin/com/iurii/validationdemo/model/Customer.kt
@@ -3,6 +3,7 @@ package com.iurii.validationdemo.model
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
 data class Customer(
@@ -13,4 +14,6 @@ data class Customer(
     @field:Min(value = 0, message = "Must be greater than or equal to 0")
     @field:Max(value = 10, message = "Must be less than or equal to 10")
     var freePasses: Int? = null,
+    @field:Pattern(regexp = "^[a-zA-Z0-9]{5}$", message = "Only 5 digits/letters")
+    var postalCode: String? = null,
 )
